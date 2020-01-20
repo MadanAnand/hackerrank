88b18349477df04d98597cb3f875d7ea756fd799
MadanAnand <mailidofmadan@gmail.com>
2020-01-20 23:34:28 +0100

    Removed 2 loops i.e O(n^2) to O(n).

--- DiagonalDifference.java
+++ DiagonalDifference.java
@@ -20,20 +20,13 @@ class Result {
      */
 
     public static int diagonalDifference(List<List<Integer>> arr) {
-        int d1=0;
-        int d2=0;
-            for(int i=0; i<arr.size(); i++){
-                for(int j=0;j<arr.size();j++){
-                    if(i==j){
-                        d1+=(arr.get(i)).get(j);
-                    }
-                    if(i+j == arr.size() -1){
-                        d2+=(arr.get(i)).get(j);
-                    }
-                }
-            
+    int d1=0;
+    int d2=0;
+    int sizeOfMatrix=arr.size();
+        for(int i=0; i<sizeOfMatrix; i++){
+                d1+=(arr.get(i)).get(i);                  //00, 11, 22
+                d2+=(arr.get(i)).get(sizeOfMatrix -i -1); //02, 01, 00
             }
-
     return Math.abs(d1 - d2);
     }
 
